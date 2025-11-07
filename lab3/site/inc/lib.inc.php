5684324535eb0cf5d1b4d33081bba38e8b63c4fd
ZerxFox <36778239+zerxfox@users.noreply.github.com>
2025-11-07 10:08:48 +0300

    Update lib.inc.php

--- lab3/site/inc/lib.inc.php
+++ lab3/site/inc/lib.inc.php
@@ -1,31 +1,59 @@
 <?php
-function getTable($rows = 5, $cols = 5, $color = 'lightblue') {
-    echo "<table border='1' width='200'>";
-    for ($tr = 1; $tr <= $rows; $tr++) {
-        echo "<tr>";
-        for ($td = 1; $td <= $cols; $td++) {
-            if ($tr == 1 || $td == 1) {
-                echo "<th bgcolor='$color'>" . $tr * $td . "</th>";
-            } else {
-                echo "<td>" . $tr * $td . "</td>";
-            }
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-}
+declare(strict_types=1);
 
-function getMenu($menu, $vertical = true) {
-    if (!$vertical) {
-        $style = "display: inline-block; margin-right: 10px;";
-    } else {
-        $style = "";
+/**
+ * Генерирует HTML меню из массива
+ * 
+ * @param array $menu Массив пунктов меню
+ * @param bool $vertical Флаг вертикального отображения (true - вертикально, false - горизонтально)
+ * @return string HTML код меню
+ */
+function getMenu(array $menu, bool $vertical = true): string
+{
+    $cssClass = $vertical ? 'menu vertical' : 'menu horizontal';
+    $html = "<ul class=\"$cssClass\">";
+    
+    foreach ($menu as $menuItem) {
+        $html .= "<li><a href='{$menuItem['href']}'>{$menuItem['link']}</a></li>";
     }
     
-    echo "<ul>";
-    foreach ($menu as $item) {
-        echo "<li style='$style'><a href='{$item['href']}'>{$item['link']}</a></li>";
+    $html .= '</ul>';
+    return $html;
+}
+
+/**
+ * Генерирует HTML таблицу умножения
+ * 
+ * @param int $cols Количество столбцов (по умолчанию 5)
+ * @param int $rows Количество строк (по умолчанию 5)
+ * @param string $color Цвет фона заголовков (по умолчанию 'yellow')
+ * @return int Количество вызовов функции
+ */
+function getTable(int $cols = 5, int $rows = 5, string $color = 'yellow'): int
+{
+    static $count = 0;
+    $count++;
+    
+    $html = "<h3>Таблица {$rows}×{$cols} (цвет: $color)</h3>";
+    $html .= '<table>';
+    
+    // Генерируем таблицу умножения
+    for ($i = 1; $i <= $rows; $i++) {
+        $html .= '<tr>';
+        for ($j = 1; $j <= $cols; $j++) {
+            // Первая строка и первый столбец - заголовки
+            if ($i === 1 || $j === 1) {
+                $html .= "<th style='background-color: $color;'>" . ($i * $j) . "</th>";
+            } else {
+                $html .= "<td>" . ($i * $j) . "</td>";
+            }
+        }
+        $html .= '</tr>';
     }
-    echo "</ul>";
+    
+    $html .= '</table>';
+    echo $html;
+    
+    return $count;
 }
 ?>
