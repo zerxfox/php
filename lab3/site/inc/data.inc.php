14e6d14c284042788eddd9d4437994381e09ae78
ZerxFox <36778239+zerxfox@users.noreply.github.com>
2025-11-07 10:08:22 +0300

    Update data.inc.php

--- lab3/site/inc/data.inc.php
+++ lab3/site/inc/data.inc.php
@@ -1,10 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$cols = $_GET['cols'] ?? 5;
-$rows = $_GET['rows'] ?? 7;
-$color = $_GET['color'] ??  'lightgreen';
-
+// Данные для функции getMenu()
 $menu = [
     ['href' => 'index.php', 'link' => 'Домой'],
     ['href' => 'about.php', 'link' => 'О нас'],
@@ -12,4 +9,9 @@ $menu = [
     ['href' => 'table.php', 'link' => 'Таблица умножения'],
     ['href' => 'calc.php', 'link' => 'Калькулятор']
 ];
+
+// Параметры по умолчанию для функции getTable()
+$cols = $_GET['cols'] ?? 5;
+$rows = $_GET['rows'] ?? 7;
+$color = $_GET['color'] ??  'lightgreen';
 ?>
