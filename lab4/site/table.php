48fccea00a3acae061ba71358eee89cc87e6d9ac
ZerxFox <36778239+zerxfox@users.noreply.github.com>
2025-11-07 11:34:22 +0300

    Update table.php

--- lab4/site/table.php
+++ lab4/site/table.php
@@ -1,32 +1,44 @@
 <?php
+// Устанавливаем значения по умолчанию
+$default_cols = 5;
+$default_rows = 7;
+$default_color = '#90ee90';
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $cols = abs((int) $_POST['cols']);
     $rows = abs((int) $_POST['rows']);
     $color = trim(strip_tags($_POST['color']));
 }
-$cols = ($cols) ? $cols : 10;
-$rows = ($rows) ? $rows : 10;
-$color = ($color) ? $color : '#ffff00';
+
+$cols = ($cols) ? $cols : $default_cols;
+$rows = ($rows) ? $rows : $default_rows;
+$color = ($color) ? $color : $default_color;
 ?>
 <!-- Область основного контента -->
 <h3>Таблица умножения</h3>
 <form action='<?=$_SERVER['REQUEST_URI']?>' method='POST'>
     <label>Количество колонок: </label>
     <br>
-    <input name='cols' type='text' value='<?=isset($_POST['cols']) ? $_POST['cols'] : ''?>'>
+    <input name='cols' type='text' value='<?=isset($_POST['cols']) ? htmlspecialchars($_POST['cols']) : $default_cols?>'>
     <br>
     <label>Количество строк: </label>
     <br>
-    <input name='rows' type='text' value='<?=isset($_POST['rows']) ? $_POST['rows'] : ''?>'>
+    <input name='rows' type='text' value='<?=isset($_POST['rows']) ? htmlspecialchars($_POST['rows']) : $default_rows?>'>
     <br>
     <label>Цвет: </label>
     <br>
-    <input name='color' type='color' value='<?=isset($_POST['color']) ? $_POST['color'] : '#ff0000'?>' list="listColors">
-    <datalist id="listColors">
-        <option>#ff0000</option>
-        <option>#00ff00</option>
-        <option>#0000ff</option>
-    </datalist>
+    <input name='color' type='color' value='<?=isset($_POST['color']) ? htmlspecialchars($_POST['color']) : $default_color?>'>
+    <br>
+    <small>Или выберите из списка: </small>
+    <select onchange="document.querySelector('input[name=color]').value = this.value">
+        <option value="#90ee90">Светло-зеленый</option>
+        <option value="#ff0000">Красный</option>
+        <option value="#00ff00">Зеленый</option>
+        <option value="#0000ff">Синий</option>
+        <option value="#ffff00">Желтый</option>
+        <option value="#ff00ff">Пурпурный</option>
+        <option value="#00ffff">Бирюзовый</option>
+    </select>
     <br>
     <br>
     <input type='submit' value='Создать'>
